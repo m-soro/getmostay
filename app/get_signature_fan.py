d9aba04f52602455f8599949a5af3bae7dadcc0a
mark-soro <napoleon_soro@rocketmail.com>
2022-05-29 15:33:50 -0400

    added the 3 missing fan/logo pictures

--- app/get_signature_fan.py
+++ app/get_signature_fan.py
@@ -40,5 +40,16 @@ def get_signature_fan():
                                         ]
 
     destination = random.choice(destinations)
-    signature_fan = "https://photos.mandarinoriental.com/is/image/MandarinOriental/" + destination + "-hotel-signature-fan?"
-    return signature_fan, destination
+
+    if destination == 'abu-dhabi':
+        signature_fan = "https://byconnoisseur.com/wp-content/uploads/2019/05/Emirates-Palace-Hotel.jpg"
+        return signature_fan, destination
+    elif destination == 'riyadh':
+        signature_fan = "https://a.mktgcdn.com/p/wTR1Ao1HyvL2YOOkx874IAekgPcshGB4oarQLJAvdlM/2066x2066.jpg"
+        return signature_fan, destination
+    elif destination == 'prague':
+        signature_fan = "https://photos.mandarinoriental.com/is/image/MandarinOriental/prauge-hotel-signature-fan?"
+        return signature_fan, destination
+    else:
+        signature_fan = "https://photos.mandarinoriental.com/is/image/MandarinOriental/" + destination + "-hotel-signature-fan?"
+        return signature_fan, destination
